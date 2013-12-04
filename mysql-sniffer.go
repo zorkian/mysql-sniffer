153d729178340c34d8752fafd3a744224368da3c
Mark Smith <zorkian@dropbox.com>
2013-12-04 02:03:01 -0800

    Major update to add timing info
    This adds timing information to the MySQL sniffer, plus some better
    formatting output options.

--- mysql-sniffer.go
+++ mysql-sniffer.go
@@ -22,42 +22,99 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/akrennmair/gopcap"
 	"log"
-	"pcap"
+	"math/rand"
 	"sort"
 	"strings"
 	"time"
 )
 
-var start int64 = UnixNow()
-var qbuf map[string]int = make(map[string]int)
-var querycount int
+const (
+	TOKEN_DEFAULT    = 0
+	TOKEN_QUOTE      = 1
+	TOKEN_NUMBER     = 2
+	TOKEN_WHITESPACE = 3
 
-const TOKEN_DEFAULT = 0
-const TOKEN_QUOTE = 1
-const TOKEN_NUMBER = 2
-const TOKEN_WHITESPACE = 3
+	// MySQL packet types
+	COM_QUERY = 3
+
+	// These are used for formatting outputs
+	F_NONE = iota
+	F_QUERY
+	F_ROUTE
+	F_SOURCE
+	F_SOURCEIP
+)
+
+type packet struct {
+	request bool // request or response
+	data    []byte
+}
+
+type source struct {
+	src       string
+	srcip     string
+	synced    bool
+	reqbuffer []byte
+	resbuffer []byte
+	reqSent   *time.Time
+	reqTimes  [100]uint64
+	qbytes    uint64
+	qdata     *queryData
+	qtext     string
+}
+
+type queryData struct {
+	count uint64
+	bytes uint64
+	times [100]uint64
+}
+
+var start int64 = UnixNow()
+var qbuf map[string]*queryData = make(map[string]*queryData)
+var querycount int
+var chmap map[string]*source = make(map[string]*source)
+var verbose bool = false
+var dirty bool = false
+var format []interface{}
+var port uint16
+var times [100]uint64
+
+var stats struct {
+	packets struct {
+		rcvd      uint64
+		rcvd_sync uint64
+	}
+	desyncs uint64
+	streams uint64
+}
 
 func UnixNow() int64 {
 	return time.Now().Unix()
 }
 
 func main() {
-	var port *int = flag.Int("P", 3306, "MySQL port to use")
+	var lport *int = flag.Int("P", 3306, "MySQL port to use")
 	var eth *string = flag.String("i", "eth0", "Interface to sniff")
-	var snaplen *int = flag.Int("s", 1024, "Bytes of each packet to sniff")
-	var dirty *bool = flag.Bool("u", false, "Unsanitized -- do not canonicalize queries")
-	var binary *bool = flag.Bool("b", false, "Output binary -- do not escape binary in queries")
+	var ldirty *bool = flag.Bool("u", false, "Unsanitized -- do not canonicalize queries")
 	var period *int = flag.Int("t", 10, "Seconds between outputting status")
-	var displaycount *int = flag.Int("d", 25, "Display this many queries in status updates")
-	var verbose *bool = flag.Bool("v", false, "Print every query received (spammy)")
+	var displaycount *int = flag.Int("d", 15, "Display this many queries in status updates")
+	var doverbose *bool = flag.Bool("v", false, "Print every query received (spammy)")
+	var formatstr *string = flag.String("f", "#q", "Format for output aggregation")
 	flag.Parse()
+
+	verbose = *doverbose
+	port = uint16(*lport)
+	dirty = *ldirty
+	parseFormat(*formatstr)
+	rand.Seed(time.Now().UnixNano())
 
 	log.SetPrefix("")
 	log.SetFlags(0)
 
-	log.Printf("Initializing MySQL sniffing on %s:%d...", *eth, *port)
-	iface, err := pcap.Openlive(*eth, int32(*snaplen), false, 0)
+	log.Printf("Initializing MySQL sniffing on %s:%d...", *eth, port)
+	iface, err := pcap.Openlive(*eth, 1024, false, 0)
 	if iface == nil || err != nil {
 		msg := "unknown error"
 		if err != nil {
@@ -66,7 +123,7 @@ func main() {
 		log.Fatalf("Failed to open device: %s", msg)
 	}
 
-	err = iface.Setfilter(fmt.Sprintf("tcp dst port %d", *port))
+	err = iface.Setfilter(fmt.Sprintf("tcp port %d", port))
 	if err != nil {
 		log.Fatalf("Failed to set port filter: %s", err.Error())
 	}
@@ -77,17 +134,43 @@ func main() {
 
 	for rv = 0; rv >= 0; {
 		for pkt, rv = iface.NextEx(); pkt != nil; pkt, rv = iface.NextEx() {
-			handlePacket(pkt, *dirty, *binary, *verbose)
+			handlePacket(pkt)
 
 			// simple output printer... this should be super fast since we expect that a
 			// system like this will have relatively few unique queries once they're
 			// canonicalized.
-			if !*verbose && querycount%100 == 0 && last < UnixNow()-int64(*period) {
+			if !verbose && querycount%100 == 0 && last < UnixNow()-int64(*period) {
 				last = UnixNow()
 				handleStatusUpdate(*displaycount)
 			}
 		}
 	}
+}
+
+func calculateTimes(timings *[100]uint64) (fmin, favg, fmax float64) {
+	var counts, total, min, max, avg uint64 = 0, 0, 0, 0, 0
+	has_min := false
+	for _, val := range *timings {
+		if val == 0 {
+			// Queries should never take 0 nanoseconds. We are using 0 as a
+			// trigger to mean 'uninitialized reading'.
+			continue
+		}
+		if val < min || !has_min {
+			has_min = true
+			min = val
+		}
+		if val > max {
+			max = val
+		}
+		counts++
+		total += val
+	}
+	if counts > 0 {
+		avg = total / counts // integer division
+	}
+	return float64(min) / 1000000, float64(avg) / 1000000,
+		float64(max) / 1000000
 }
 
 func handleStatusUpdate(displaycount int) {
@@ -96,13 +179,26 @@ func handleStatusUpdate(displaycount int) {
 	// print status bar
 	log.Printf("\n")
 	log.SetFlags(log.Ldate | log.Ltime)
-	log.Printf("%d total queries, %0.2f per second", querycount, float64(querycount)/elapsed)
+	log.Printf("%d total queries, %0.2f per second", querycount,
+		float64(querycount)/elapsed)
 	log.SetFlags(0)
+
+	log.Printf("%d packets (%0.2f%% on synchronized streams) / %d desyncs / %d streams",
+		stats.packets.rcvd, float64(stats.packets.rcvd_sync)/float64(stats.packets.rcvd)*100,
+		stats.desyncs, stats.streams)
+
+	// global timing values
+	gmin, gavg, gmax := calculateTimes(&times)
+	log.Printf("%0.2fms min / %0.2fms avg / %0.2fms max query time",
+		gmin, gavg, gmax)
+	log.Printf(" ")
 
 	// we cheat so badly here...
 	var tmp sort.StringSlice = make([]string, 0, len(qbuf))
 	for q, c := range qbuf {
-		tmp = append(tmp, fmt.Sprintf("%6d  %0.2f/s  %s", c, float64(c)/elapsed, q))
+		qmin, qavg, qmax := calculateTimes(&c.times)
+		tmp = append(tmp, fmt.Sprintf("%6d  %6.2f/s  %6.2f %6.2f %6.2f %8db  %s",
+			c.count, float64(c.count)/elapsed, qmin, qavg, qmax, c.bytes, q))
 	}
 	sort.Sort(tmp)
 
@@ -116,32 +212,224 @@ func handleStatusUpdate(displaycount int) {
 	}
 }
 
+// Do something with a packet for a source.
+func processPacket(rs *source, request bool, data []byte) {
+	//		log.Printf("[%s] request=%t, got %d bytes", rs.src, request,
+	//			len(data))
+
+	stats.packets.rcvd++
+	if rs.synced {
+		stats.packets.rcvd_sync++
+	}
+
+	var ptype int = -1
+	var pdata []byte
+
+	if request {
+		// If we still have response buffer, we're in some weird state and
+		// didn't successfully process the response.
+		if rs.resbuffer != nil {
+			//				log.Printf("[%s] possibly pipelined request? %d bytes",
+			//					rs.src, len(rs.resbuffer))
+			stats.desyncs++
+			rs.resbuffer = nil
+			rs.synced = false
+		}
+		rs.reqbuffer = data
+		ptype, pdata = carvePacket(&rs.reqbuffer)
+	} else {
+		rs.resbuffer = data
+		ptype, pdata = carvePacket(&rs.resbuffer)
+	}
+
+	// The synchronization logic: if we're not presently, then we want to
+	// keep going until we are capable of carving off of a request/query.
+	if !rs.synced {
+		if !(request && ptype == COM_QUERY) {
+			rs.reqbuffer, rs.resbuffer = nil, nil
+			return
+		}
+		rs.synced = true
+	}
+	//log.Printf("[%s] request=%b ptype=%d plen=%d", rs.src, request, ptype, len(pdata))
+
+	// No (full) packet detected yet. Continue on our way.
+	if ptype == -1 {
+		return
+	}
+	plen := uint64(len(pdata))
+
+	// If this is a response then we want to record the timing and
+	// store it with this channel so we can keep track of that.
+	var reqtime uint64
+	if !request {
+		if rs.reqSent == nil {
+			return
+		}
+		reqtime = uint64(time.Since(*rs.reqSent).Nanoseconds())
+
+		// We keep track of per-source, global, and per-query timings.
+		randn := rand.Intn(100)
+		rs.reqTimes[randn] = reqtime
+		times[randn] = reqtime
+		if rs.qdata != nil {
+			// This should never fail but it has. Probably because of a
+			// race condition I need to suss out, or sharing between
+			// two different goroutines. :(
+			rs.qdata.times[randn] = reqtime
+			rs.qdata.bytes += plen
+		}
+		rs.reqSent = nil
+
+		// If we're in verbose mode, just dump statistics from this one.
+		if verbose {
+			log.Printf("%s %d %d %0.2f\n", rs.qtext, rs.qbytes, plen,
+				float64(reqtime)/1000000)
+		}
+
+		return
+	}
+
+	// This is for sure a request, so let's count it as one.
+	if rs.reqSent != nil {
+		//			log.Printf("[%s] ...sending two requests without a response?",
+		//				rs.src)
+	}
+	tnow := time.Now()
+	rs.reqSent = &tnow
+
+	// Convert this request into whatever format the user wants.
+	querycount++
+	var text string
+
+	/*if dirty {
+		text += string(pdata)
+	} else {
+		text += cleanupQuery(pdata)
+	}*/
+
+	for _, item := range format {
+		switch item.(type) {
+		case int:
+			switch item.(int) {
+			case F_NONE:
+				log.Fatalf("F_NONE in format string")
+			case F_QUERY:
+				if dirty {
+					text += string(pdata)
+				} else {
+					text += cleanupQuery(pdata)
+				}
+			case F_ROUTE:
+				//
+			case F_SOURCE:
+				text += rs.src
+			case F_SOURCEIP:
+				text += rs.srcip
+			default:
+				log.Fatalf("Unknown F_XXXXXX int in format string")
+			}
+		case string:
+			text += item.(string)
+		default:
+			log.Fatalf("Unknown type in format string")
+		}
+	}
+	qdata, ok := qbuf[text]
+	if !ok {
+		qdata = &queryData{}
+		qbuf[text] = qdata
+	}
+	qdata.count++
+	qdata.bytes += plen
+	rs.qtext, rs.qdata, rs.qbytes = text, qdata, plen
+}
+
+// carvePacket tries to pull a packet out of a slice of bytes. If so, it removes
+// those bytes from the slice.
+func carvePacket(buf *[]byte) (int, []byte) {
+	datalen := uint32(len(*buf))
+	if datalen < 5 {
+		return -1, nil
+	}
+
+	size := uint32((*buf)[0]) + uint32((*buf)[1])<<8 + uint32((*buf)[2])<<16
+	if size == 0 || datalen < size+4 {
+		return -1, nil
+	}
+
+	// Else, has some length, try to validate it.
+	end := size + 4
+	ptype := int((*buf)[4])
+	data := (*buf)[5 : size+4]
+	if end >= datalen {
+		*buf = nil
+	} else {
+		*buf = (*buf)[end:]
+	}
+
+	//	log.Printf("datalen=%d size=%d end=%d ptype=%d data=%d buf=%d",
+	//		datalen, size, end, ptype, len(data), len(*buf))
+
+	return ptype, data
+}
+
 // extract the data... we have to figure out where it is, which means extracting data
 // from the various headers until we get the location we want.  this is crude, but
 // functional and it should be fast.
-func handlePacket(pkt *pcap.Packet, dirty, binary, verbose bool) {
+func handlePacket(pkt *pcap.Packet) {
 	// Ethernet frame has 14 bytes of stuff to ignore, so we start our root position here
-	var pos uint8 = 14
+	var pos byte = 14
 
-	// The IP frame has the header length in bits 4-7 of byte 0 (relative)
+	// Grab the src IP address of this packet from the IP header.
+	srcIP := pkt.Data[pos+12 : pos+16]
+	dstIP := pkt.Data[pos+16 : pos+20]
+
+	// The IP frame has the header length in bits 4-7 of byte 0 (relative).
 	pos += pkt.Data[pos] & 0x0F * 4
 
-	// The TCP frame has the data offset in bits 4-7 of byte 12 (relative)
-	pos += uint8(pkt.Data[pos+12]) >> 4 * 4
+	// Grab the source port from the TCP header.
+	srcPort := uint16(pkt.Data[pos])<<8 + uint16(pkt.Data[pos+1])
+	dstPort := uint16(pkt.Data[pos+2])<<8 + uint16(pkt.Data[pos+3])
 
-	// must be at least 6 bytes of payload
-	if len(pkt.Data)-int(pos) <= 6 {
+	// The TCP frame has the data offset in bits 4-7 of byte 12 (relative).
+	pos += byte(pkt.Data[pos+12]) >> 4 * 4
+
+	// If this is a 0-length payload, do nothing. (Any way to change our filter
+	// to only dump packets with data?)
+	if len(pkt.Data[pos:]) <= 0 {
 		return
 	}
 
-	// MySQL packet type is the fifth byte... COM_QUERY is 0x03, if it's not that
-	// then we don't care, move on
-	if pkt.Data[pos+4] != 0x03 {
-		return
+	// This is either an inbound or outbound packet. Determine by seeing which
+	// end contains our port. Either way, we want to put this on the channel of
+	// the remote end.
+	var src string
+	var request bool = false
+	if srcPort == port {
+		src = fmt.Sprintf("%d.%d.%d.%d:%d", dstIP[0], dstIP[1], dstIP[2],
+			dstIP[3], dstPort)
+		//log.Printf("response to %s", src)
+	} else if dstPort == port {
+		src = fmt.Sprintf("%d.%d.%d.%d:%d", srcIP[0], srcIP[1], srcIP[2],
+			srcIP[3], srcPort)
+		request = true
+		//log.Printf("request from %s", src)
+	} else {
+		log.Fatalf("got packet src = %d, dst = %d", srcPort, dstPort)
 	}
 
-	// the query is now in query ... easier to deal with than always offsetting
-	handleQuery(pkt.Data[pos+5:], dirty, binary, verbose)
+	// Get the data structure for this source, then do something.
+	rs, ok := chmap[src]
+	if !ok {
+		srcip := src[0:strings.Index(src, ":")]
+		rs = &source{src: src, srcip: srcip, synced: false}
+		stats.streams++
+		chmap[src] = rs
+	}
+
+	// Now with a source, process the packet.
+	processPacket(rs, request, pkt.Data[pos:])
 }
 
 // scans forward in the query given the current type and returns when we encounter
@@ -195,7 +483,11 @@ func scanToken(query []byte) (length int, thistype int) {
 	default:
 		for i := 1; i < len(query); i++ {
 			switch {
-			case query[i] == 39 || query[i] == 34 || (query[i] >= 48 && query[i] <= 57) || query[i] == 32 || (query[i] >= 9 && query[i] <= 13):
+			case query[i] >= 48 && query[i] <= 57:
+				// Numbers, allow.
+			case query[i] == 39 || query[i] == 34 || (query[i] >= 48 && query[i] <= 57) ||
+				query[i] == 32 || (query[i] >= 9 && query[i] <= 13):
+				// Certain punctuation ends our run!
 				return i, TOKEN_DEFAULT
 			default:
 			}
@@ -206,20 +498,6 @@ func scanToken(query []byte) (length int, thistype int) {
 	// shouldn't get here
 	log.Fatalf("scanToken failure: [%s]", query)
 	return
-}
-
-func cleanupRawQuery(query []byte) string {
-	var qspace []string
-	var theByte byte
-	for i := 0; i < len(query); i++ {
-		theByte = query[i]
-		if (theByte >= 0x20 && theByte <= 0x7E) || theByte == 0x0A || theByte == 0x0D {
-			qspace = append(qspace, string(theByte))
-		} else {
-			qspace = append(qspace, fmt.Sprintf("\\x%02x", theByte))
-		}
-	}
-	return strings.Join(qspace, "")
 }
 
 func cleanupQuery(query []byte) string {
@@ -249,23 +527,58 @@ func cleanupQuery(query []byte) string {
 	return strings.Join(qspace, "")
 }
 
-func handleQuery(query []byte, dirty bool, binary bool, verbose bool) {
-	var qstr string
-	if dirty && binary {
-		qstr = string(query)
-	} else if dirty {
-		qstr = cleanupRawQuery(query)
-	} else {
-		qstr = cleanupQuery(query)
+// parseFormat takes a string and parses it out into the given format slice
+// that we later use to build up a string. This might actually be an overcomplicated
+// solution?
+func parseFormat(formatstr string) {
+	formatstr = strings.TrimSpace(formatstr)
+	if formatstr == "" {
+		formatstr = "#b:#k"
 	}
 
-	// if verbose, just print it
-	if verbose {
-		log.Print(qstr)
-		return
-	}
+	is_special := false
+	curstr := ""
+	do_append := F_NONE
+	for _, char := range formatstr {
+		if char == '#' {
+			if is_special {
+				curstr += string(char)
+				is_special = false
+			} else {
+				is_special = true
+			}
+			continue
+		}
 
-	// map for later...
-	querycount++
-	qbuf[qstr]++
+		if is_special {
+			switch strings.ToLower(string(char)) {
+			case "s":
+				do_append = F_SOURCE
+			case "i":
+				do_append = F_SOURCEIP
+			case "r":
+				do_append = F_ROUTE
+			case "q":
+				do_append = F_QUERY
+			default:
+				curstr += "#" + string(char)
+			}
+			is_special = false
+		} else {
+			curstr += string(char)
+		}
+
+		if do_append != F_NONE {
+			if curstr != "" {
+				format = append(format, curstr, do_append)
+				curstr = ""
+			} else {
+				format = append(format, do_append)
+			}
+			do_append = F_NONE
+		}
+	}
+	if curstr != "" {
+		format = append(format, curstr)
+	}
 }
